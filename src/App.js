11566df7166f94129e92dca53912a0ffdbe0be12
slowCoder <1420418602@qq.com>
2020-06-29 15:01:23 +0800

    app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Router, Route, Switch } from 'react-router'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        app
+      </div>
+    )
+  }
+}
